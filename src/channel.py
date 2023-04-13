4a3d343faf9f8761be0ae40a26e5e00b027a3bc9
Kira Stepanova <stepanovakira99@gmail.com>
2023-04-13 14:01:38 +0300

    correct channel

--- src/channel.py
+++ src/channel.py
@@ -1,5 +1,10 @@
 import json
-from helper.youtube_api_manual import youtube
+import os
+from googleapiclient.discovery import build
+
+
+api_key: str = os.getenv('YT_API_KEY')
+youtube = build('youtube', 'v3', developerKey=api_key)
 
 
 class Channel:
